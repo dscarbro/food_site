2d116801bd5d41a80070920adf8f83a5e541538e
OpenShift guest <dscarbro@bulldogs.mgccc.edu>
2013-02-15 23:16:16 -0500

    Change .html to .php

---
+++ blueberrycrunch.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Blueberry Crunch</title>
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+<div id="wrapper">
+<header>
+<nav>
+	<ul>
+    	<li><a href="index.php">Home</a></li>
+    	<li><a href="pecanpiemuffins.php">Pecan Pie Muffins</a></li>
+    	<li><a href="blueberrycrunch.php">Blueberry Crunch</a></li>
+    	<li><a href="about.php">About</a></li>
+    	<li><a href="contact.php">Contact</a></li>
+	</ul>
+</nav>
+</header>
+
+<div id="content">
+</div>
+
+<footer>
+&copy; 2013 | All rights reserved
+</footer>
+
+
+</div>
+</body>
+</html>
